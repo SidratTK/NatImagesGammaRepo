a6e1866e466f575d7cad8219b91671dfefb62649
SidratTK <sidrattasawoor32@gmail.com>
2023-10-24 07:13:37 +0200

    brief description

---
+++ README.m
@@ -0,0 +1,20 @@
+% READ ME
+% These codes are associated with the paper
+% "Gamma responses to colored natural stimuli can be predicted from local low-level stimulus features."
+% Authors: Sidrat Tasawoor Kanth, Supratim Ray.
+
+% Instructions:
+% Unzip folder in working directory.
+% data, savedData folders should be withing the working directory
+% set 'folderSourceString' as working directory
+
+% showR2All performs separability analysis - Figs 1,2
+% getGammaParamters obtains model paramters from parametric data. Figs 3,4
+% getPredictedGamma has the obtained model paramters and can be directly
+% used to predict gamma given a set of features (of grating or hue patch)
+% getTypicalElecFig can be used to show image patches and approximations of
+% a typical electrode, and the gamma response estimates of that electrode. Fig5
+% getCorrelationFig uses all protocols and electrodes to get correlation
+% between actual and estimated gamma of both monkeys. 
+
+% STK 2023
